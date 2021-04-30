8796b11b1f632cac0c92997202dbfee04703c770
Nate Dickson <nate@natedickson.com>
2021-04-30 13:46:12 -0600

    Updated some git alises.

--- home/.config/fish/functions/gaa.fish
+++ home/.config/fish/functions/gaa.fish
@@ -1,4 +1,4 @@
 # Defined via `source`
-function gaa --wraps='git add all' --description 'alias gaa git add all'
-  git add all $argv; 
+function gaa --wraps='git add all' --wraps='git add --all' --description 'alias gaa git add --all'
+  git add --all $argv; 
 end
